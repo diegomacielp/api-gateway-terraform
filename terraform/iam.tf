94739a1e0a196bbe49998ec3526304fe6a455f26
diegomacieltallos <diego.maciel@tallos.com.br>
2023-01-17 13:44:01 -0300

    Terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,6 @@
 data "aws_iam_policy_document" "ecs_tasks_execution_role" {
   statement {
     actions = ["sts:AssumeRole"]
-
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
